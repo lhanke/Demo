32a9cdb75de21aaaa5696ef37c151b7b3dd6649d
Lukas Hanke <hanke@yatta.de>
2017-10-22 11:51:26 +0200

    added TestNG

---
+++ demo/src/test/java/de/lhanke/demo/TestNGTest.java
@@ -0,0 +1,12 @@
+package de.lhanke.demo;
+
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+
+	@Test
+	public void testNothing() {
+		System.out.println("Tested nothing!");
+	}
+
+}
